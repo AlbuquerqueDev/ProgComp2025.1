601eeb8349bfddeae81414bb7e2fa39284c118fe
Pedro Albuquerque <a7buquerque@gmail.com>
2025-08-04 22:19:42 -0300

    Finalizando q2 palindromos.

---
+++ Unidade02-Avaliacao01/q2.py
@@ -0,0 +1,22 @@
+def contar_palindromos():
+    contador = 0
+    for n in range(10, 100001):
+        if palindromo(n):
+            contador += 1
+    return contador
+
+
+def palindromo(numero):
+    numero_original = numero
+    numero_palindromo = 0
+    while numero > 0:
+        digito = numero % 10
+        numero_palindromo = (numero_palindromo * 10) + digito
+        numero //= 10
+
+    if numero_original == numero_palindromo:
+        return True
+
+
+quantidade = contar_palindromos()
+print(f"Existem {quantidade} números palindromos entre 10 e 100000.")
